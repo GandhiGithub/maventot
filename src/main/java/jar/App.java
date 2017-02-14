ccfc90fc65f0f7c2908cdddb5f869dddb1644f31
Nuvvula <Nuvvula>
2017-02-14 18:03:57 +0530

    maventotal

---
+++ src/main/java/jar/App.java
@@ -0,0 +1,15 @@
+package jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( "Hi gandhi!" );
+		System.out.println( "Welcome to maven!" );
+    }
+}
